5be97b3cb5ad8efd9f21129ab4cb6531acb5e9e3
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-10-21 18:05:11 +0530

    Notification with Workmanager

--- NotificationWithWorkManger/app/src/main/java/com/example/notificationwithworkmanger/MainActivity.java
+++ NotificationWithWorkManger/app/src/main/java/com/example/notificationwithworkmanger/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.notificationwithworkmanger;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
 
 import android.app.Notification;
 import android.app.NotificationChannel;
@@ -13,40 +16,49 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
-    NotificationManager nm;
+   /* NotificationManager nm;
     NotificationCompat.Builder builder;
     public static final int ID = 13134443;
     public static final String CHANNEL_ID = "muni";
 PendingIntent pi;
-Intent intent;
+Intent intent;*/
+   OneTimeWorkRequest onetime;
+   PeriodicWorkRequest secondtime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        onetime=new OneTimeWorkRequest.Builder(FirstTime.class).build();
+        secondtime=new PeriodicWorkRequest.Builder(SecondTime.class,15, TimeUnit.MINUTES).build();
+        /*nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         createChannel();
         intent=new Intent(this,MainActivity.class);
-        pi=PendingIntent.getActivity(this,ID,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        pi=PendingIntent.getActivity(this,ID,intent,PendingIntent.FLAG_UPDATE_CURRENT);*/
     }
 
-    private void createChannel() {
+    /*private void createChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel nc = new NotificationChannel(CHANNEL_ID, "apssdc", NotificationManager.IMPORTANCE_HIGH);
             nc.setLightColor(Color.BLUE);
             nc.enableVibration(true);
             nm.createNotificationChannel(nc);
         }
-    }
+    }*/
 
     public void showNotification(View view) {
-        builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        WorkManager.getInstance(this).enqueue(onetime);
+        WorkManager.getInstance(this).enqueue(secondtime);
+       /* builder = new NotificationCompat.Builder(this, CHANNEL_ID);
         builder.setContentTitle("Apssdc Notification");
         builder.setContentText("Hello Don't sit IDEL in these meeting learn something");
         builder.setSmallIcon(R.drawable.ic_baseline_notifications_24);
         builder.setDefaults(Notification.DEFAULT_ALL);
         builder.setContentIntent(pi);
         builder.addAction(R.drawable.ic_baseline_reply_24,"Reply",pi);
-        nm.notify(ID, builder.build());
+        nm.notify(ID, builder.build());*/
     }
 }
---
+++ NotificationWithWorkManger/app/src/main/java/com/example/notificationwithworkmanger/SecondTime.java
@@ -0,0 +1,60 @@
+package com.example.notificationwithworkmanger;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+public class SecondTime extends Worker {
+    NotificationManager nm;
+    NotificationCompat.Builder builder;
+    public static final int ID = 13134443;
+    public static final String CHANNEL_ID = "muni";
+    PendingIntent pi;
+    Intent intent;
+
+    public SecondTime(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        nm = (NotificationManager)getApplicationContext(). getSystemService(NOTIFICATION_SERVICE);
+        createChannel();
+        intent=new Intent(getApplicationContext(),MainActivity.class);
+        pi=PendingIntent.getActivity(getApplicationContext(),ID,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        showNotification();
+        return Result.success();
+    }
+    private void createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel nc = new NotificationChannel(CHANNEL_ID, "apssdc", NotificationManager.IMPORTANCE_HIGH);
+            nc.setLightColor(Color.BLUE);
+            nc.enableVibration(true);
+            nm.createNotificationChannel(nc);
+        }
+
+    }
+    public void showNotification() {
+        builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+        builder.setContentTitle("Apssdc Notification");
+        builder.setContentText("Hello Don't sit IDEL in these meeting learn something");
+        builder.setSmallIcon(R.drawable.ic_baseline_notifications_24);
+        builder.setDefaults(Notification.DEFAULT_ALL);
+        builder.setContentIntent(pi);
+        builder.addAction(R.drawable.ic_baseline_reply_24,"Reply",pi);
+        nm.notify(ID, builder.build());
+    }
+}
