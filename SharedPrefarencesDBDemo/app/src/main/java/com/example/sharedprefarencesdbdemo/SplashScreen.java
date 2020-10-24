853952c1634db20d67fccddff1fddc27af51aae7
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-10-24 17:42:07 +0530

    with splash Screen

---
+++ SharedPrefarencesDBDemo/app/src/main/java/com/example/sharedprefarencesdbdemo/SplashScreen.java
@@ -0,0 +1,38 @@
+package com.example.sharedprefarencesdbdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+    ImageView iv;
+    TextView tv;
+    /**
+     * Duration of wait
+     **/
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+       // iv = findViewById(R.id.imageView);
+        tv = findViewById(R.id.textView);
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
